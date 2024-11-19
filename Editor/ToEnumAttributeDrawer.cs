5224237d2132fd78f16e573b2cfca46735f130eb
solutena <solutena@naver.com>
2024-11-19 14:50:23 +0900

    1.0.1v - 리스트 지원

--- Editor/ToEnumAttributeDrawer.cs
+++ Editor/ToEnumAttributeDrawer.cs
@@ -5,22 +5,28 @@ using UnityEditor;
 [CustomPropertyDrawer(typeof(ToEnumAttribute))]
 public class ToEnumAttributeDrawer : PropertyDrawer
 {
-	bool Condition(SerializedProperty property)
-	{
-		return property.propertyType == SerializedPropertyType.String;
-	}
-
 	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
 	{
-		if (Condition(property) == false)
-		{
-			EditorGUI.PropertyField(position, property, label, true);
-			position.y += EditorGUIUtility.singleLineHeight;
-			position.height = EditorGUIUtility.singleLineHeight;
-			EditorGUI.HelpBox(position, "ToEnumAttribute only supports String type", MessageType.Warning);
-			return;
-		}
+		if (property.propertyType == SerializedPropertyType.String)
+			DrawEnumPopup(position, property, label);
+		else if (property.propertyType == SerializedPropertyType.Generic && property.isArray)
+			DrawArray(position, property, label);
+		else
+			DrawError(position, property, label);
+	}
 
+	void DrawArray(Rect position, SerializedProperty property, GUIContent label)
+	{
+		for (int i = 0; i < property.arraySize; i++)
+		{
+			SerializedProperty element = property.GetArrayElementAtIndex(i);
+			Rect elementRect = new(position.x, position.y + i * EditorGUIUtility.singleLineHeight, position.width, EditorGUIUtility.singleLineHeight);
+			DrawEnumPopup(elementRect, element, label);
+		}
+	}
+
+	void DrawEnumPopup(Rect position, SerializedProperty property, GUIContent label)
+	{
 		string enumString = property.stringValue;
 		ToEnumAttribute stringToEnumAttribute = (ToEnumAttribute)attribute;
 		Type enumType = stringToEnumAttribute.EnumType;
@@ -34,10 +40,21 @@ public class ToEnumAttributeDrawer : PropertyDrawer
 			property.stringValue = enumValue.ToString();
 	}
 
+	void DrawError(Rect position, SerializedProperty property, GUIContent label)
+	{
+		EditorGUI.PropertyField(position, property, label, true);
+		position.y += EditorGUIUtility.singleLineHeight;
+		position.height = EditorGUIUtility.singleLineHeight;
+		EditorGUI.HelpBox(position, "ToEnumAttribute only supports String type", MessageType.Warning);
+	}
+
 	public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
 	{
-		if (Condition(property) == false)
+		if (property.propertyType == SerializedPropertyType.String)
+			return base.GetPropertyHeight(property, label);
+		else if (property.propertyType == SerializedPropertyType.Generic && property.isArray)
+			return property.arraySize * EditorGUIUtility.singleLineHeight;
+		else
 			return base.GetPropertyHeight(property, label) + EditorGUIUtility.singleLineHeight;
-		return base.GetPropertyHeight(property, label);
 	}
 }
