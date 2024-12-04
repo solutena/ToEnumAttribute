ef5734a2e2164359b2bc7bde5d14d4e250b4b645
solutena <solutena@naver.com>
2024-12-05 03:09:39 +0900

    Delete Runtime/ToEnumExtensions.cs

--- Runtime/ToEnumExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-public static class ToEnumExtensions
-{
-	public static T ParseEnum<T>(this string enumName, T defaultValue = default) where T : struct, Enum
-	{
-		return Enum.TryParse(enumName, out T result) ? result : defaultValue;
-	}
-}
