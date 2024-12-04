49d11b72265af557b922e003fe18970c3764f0a9
solutena <solutena@naver.com>
2024-12-05 02:35:10 +0900

    Add files via upload

---
+++ Runtime/ToEnumExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public static class ToEnumExtensions
+{
+	public static T ParseEnum<T>(this string enumName, T defaultValue = default) where T : struct, Enum
+	{
+		return Enum.TryParse(enumName, out T result) ? result : defaultValue;
+	}
+}
